ffe0254d99c376fd7ee6dd72135264f0df7d0e74
inesmargarido <58304841+inesmargarido@users.noreply.github.com>
2024-05-27 17:26:46 +0100

    Add files via upload

--- avg_delivery_time.py
+++ avg_delivery_time.py
@@ -13,7 +13,7 @@ def parse_args():
 # function to read events from json file and create an array
 def read_events(input_file):
   with open(input_file, "r") as file:
-    events = [json.loads(line.strip()) for line in file] # clean any empty spaces
+    events = [json.loads(line.strip()) for line in file]  # clean any empty spaces
   return events
 
 # main function to calculate the average each minute
@@ -21,19 +21,21 @@ def calculate_moving_averages(events, window_minutes):
 
   # get only timestamp and duration of each event
   events = [(datetime.fromisoformat(event['timestamp']), event['duration']) for event in events]
-  
+
   # get the first event's timestamp (rounded down to the minute)
   start_time = events[0][0].replace(second=0, microsecond=0)
   # get the latest event's timestamp and round up to the next minute
   end_time = events[-1][0].replace(second=0, microsecond=0) + timedelta(minutes=1)
 
-  current_minute = start_time # initiate the iterator for the outside loop 
-  durations = deque() # manage durations for the calculations
-  i = 0 # iterator for events
-  current_window_count = 0 # number of durations in the current window
-  current_window_sum = 0 # sum of durations in the current window
-  avg_per_min = 0 # variable to store the average calculation
-  print_count = 0 # number of outputs already printed
+  current_minute = start_time  # initiate the iterator for the outside loop
+  durations = deque()  # manage durations for the calculations
+  i = 0  # iterator for events
+  current_window_count = 0  # number of durations in the current window
+  current_window_sum = 0  # sum of durations in the current window
+  avg_per_min = 0  # variable to store the average calculation
+  print_count = 0  # number of outputs already printed
+
+  result = {} # dictionary to save result
 
   while current_minute <= end_time:
     # add durations to the deque which correspond to translations in the current minute
@@ -42,28 +44,46 @@ def calculate_moving_averages(events, window_minutes):
       current_window_sum += events[i][1]
       current_window_count += 1
       i += 1
-    
+
     # calculate current average
-    avg_per_min = current_window_sum/current_window_count if current_window_count > 0 else 0
-  
-    # print output
-    print(f'{{"date": "{current_minute.isoformat()}", "average_delivery_time": "{avg_per_min:.1f}"}}')
+    avg_per_min = (current_window_sum/current_window_count) if current_window_count > 0 else 0
+
+    # save timestamp + average in dictionary
+    result[current_minute] = avg_per_min
 
     print_count += 1
     current_minute += timedelta(minutes=1)
 
-    # if we reach the end of the window, slide window to the right 
+    # if we reach the end of the window, slide window to the right
     if print_count > window_minutes:
       removed_duration = durations.popleft() # remove the oldest duration from the deque
-      current_window_sum -= removed_duration # remove from the sum
-      current_window_count -= 1 # remove from the count
-      print_count = 0 # reset counter
+      current_window_sum -= removed_duration  # remove from the sum
+      current_window_count -= 1  # remove from the count
+      print_count = 0  # reset counter
 
+  return result
 
 def main():
-  args = parse_args()
-  events = read_events(args.input_file)
-  moving_averages = calculate_moving_averages(events, args.window_minutes)
+  try:
+    args = parse_args()
+    events = read_events(args.input_file)
+
+    output = calculate_moving_averages(events, args.window_minutes)
+
+    # transform the output into a json file in the desired format
+    output_json = [
+      json.dumps({"date": str(current_minute), "average_delivery_time": avg_per_min}) + '\n'
+      for current_minute, avg_per_min in output.items()
+    ]
+
+    # write on the file
+    with open('output.json','w') as file:
+      file.writelines(output_json)
+      print(f"The output file was saved: output.json")
+
+  except Exception as e:
+    print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
   main()
